8997f24ae2acab139a203d550a2539c57dbefbd5
Wim Looman <ghostunderscore@gmail.com>
2014-03-15 15:42:32 +1300

    Move entity index page rendering into a base template

--- public/pages/entities.js
+++ public/pages/entities.js
@@ -10,12 +10,18 @@ define([
     ko,
     mapping
 ) {
+    var titleCase = function (str) {
+        // http://stackoverflow.com/a/196991/260593
+        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    };
+
     var EntitiesPage = function (config) {
         _.bindAll(this);
         this.entities = ko.observableArray();
         this[config.model.plural_name] = this.entities;
         this.model = config.model;
         this.url = config.url;
+        this.title = titleCase(config.model.plural_name);
     };
 
     EntitiesPage.prototype.refresh = function () {
