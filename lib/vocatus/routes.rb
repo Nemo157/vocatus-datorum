715543485fa54df342e87eebf4330cb0ca514573
Wim Looman <ghostunderscore@gmail.com>
2014-03-19 23:47:09 +1300

    Correct routes for show/edit paths (really need to make the generic routes generated...)

--- lib/vocatus/routes.rb
+++ lib/vocatus/routes.rb
@@ -5,42 +5,60 @@ module Vocatus
       'cocktails' => {
           '' => 'cocktails/index',
           'new' => 'cocktails/new',
-          ':cocktail_id' => 'cocktails/show',
-          'recipes' => {
-              '' => 'recipes/index',
-              'new' => 'recipes/new',
-              ':recipe_id' => 'recipes/show'
-          }
+          ':cocktail_id' => {
+            '' => 'cocktails/show',
+            'edit' => 'cocktails/edit',
+            'recipes' => {
+                '' => 'recipes/index',
+                'new' => 'recipes/new',
+                ':recipe_id' => 'recipes/show',
+            },
+          },
       },
       'ingredients' => {
           '' => 'ingredients/index',
           'new' => 'ingredients/new',
-          ':ingredient_id' => 'ingredients/show'
+          ':ingredient_id' => {
+            '' => 'ingredients/show',
+            'edit' => 'ingredients/edit',
+          },
       },
       'spirits' => {
           '' => 'spirits/index',
           'new' => 'spirits/new',
-          ':spirit_id' => 'spirits/show'
+          ':spirit_id' => {
+            '' => 'spirits/show',
+            'edit' => 'spirits/edit',
+          },
       },
       'mixers' => {
           '' => 'mixers/index',
           'new' => 'mixers/new',
-          ':mixer_id' => 'mixers/show'
+          ':mixer_id' => {
+            '' => 'mixers/show',
+            'edit' => 'mixers/edit',
+          },
       },
       'users' => {
           '' => 'users/index',
           'new' => 'users/new',
-          ':user_id' => 'users/show'
+          ':user_id' => {
+            '' => 'users/show',
+            'edit' => 'users/edit',
+          },
       },
       'sessions' => {
           '' => 'user_sessions/index',
           'new' => 'user_sessions/new',
-          ':user_session_id' => 'user_sessions/show'
+          ':user_session_id' => {
+            '' => 'user_sessions/show',
+            'edit' => 'user_sessions/edit',
+          },
       },
       'register' => 'users/new',
       'login' => 'user_sessions/new',
       'user' => 'users/show',
-      'session' => 'user_sessions/show'
+      'session' => 'user_sessions/show',
     }
   end
 end
