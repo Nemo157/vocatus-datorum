84d43f217bf1b61669c77231fff7ec7023146bcf
Wim Looman <ghostunderscore@gmail.com>
2014-07-30 23:48:57 +1200

    Fully fix logging in and disable button while it is

--- public/pages/user_sessions/new.js
+++ public/pages/user_sessions/new.js
@@ -3,6 +3,7 @@ define([
     'lodash',
     'knockout',
     'knockout.validation',
+    'when',
     'app',
     'models/users',
     'models/user_session'
@@ -11,34 +12,47 @@ define([
     _,
     ko,
     validation,
+    when,
     app,
     UserList,
     UserSession
 ) {
-    var loginPage = ko.validatedObservable({
+    return ko.validatedObservable({
         email: ko.observable(),
+        isProcessing: ko.observable(),
         login: function () {
+            this.isProcessing(true);
             var userList = UserList.get();
-            userList.refresh().done(function () {
-                var user = _.find(userList.items(), function (user) {
-                    return user.email() === loginPage().email();
-                });
-                if (user) {
-                    $.ajax({
-                        type: 'POST',
-                        url: user.user_sessions().uri(),
-                        data: JSON.stringify({}),
-                        contentType: 'application/json',
-                        processData: false
-                    }).done(function (data) {
-                        app.user(user);
-                        app.session(UserSession.create(data));
+            userList.refresh().then(function (userList) {
+                return when.all(_.map(userList.items(), function (item) {
+                    return item.refresh();
+                }));
+            }).done(_.bind(this.onUserListRefreshed, this));
+        },
+        onUserListRefreshed: function (users) {
+            var user = _.find(users, function (user) {
+                return user.email() === this.email();
+            }, this);
+            if (user) {
+                $.ajax({
+                    type: 'POST',
+                    url: user.user_sessions().uri(),
+                    data: JSON.stringify({}),
+                    contentType: 'application/json',
+                    processData: false
+                }).done(function (data) {
+                    var session = UserSession.create(data);
+                    app.user(user);
+                    app.session(session);
+                    when.all([user.refresh(), session.refresh()], function () {
                         app.router.redirect((app.pager.last_page() && app.pager.last_page().originalPath) || '/');
-                    });
-                }
-            });
+                        this.isProcessing(false);
+                    }, this);
+                }, this);
+            } else {
+                alert("User not found");
+                this.isProcessing(false);
+            }
         }
-    });
-
-    return loginPage();
+    })();
 });
